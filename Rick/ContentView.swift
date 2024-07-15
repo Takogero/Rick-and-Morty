04654c6321298910c99e30b3ef15bc1eb9212c55
AbrosimovAnton123 <abrosimov.a@techspaceapp.com>
2024-07-15 15:52:08 +0300

    Initial Commit

---
+++ Rick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick
+//
+//  Created by MacBookAir on 15.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rick/RickApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickApp.swift
+//  Rick
+//
+//  Created by MacBookAir on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
